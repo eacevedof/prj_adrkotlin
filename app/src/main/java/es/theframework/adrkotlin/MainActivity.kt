9a0f72b5dcd6dcdbda828fb1979d52fbbeb6c9ca
eacevedof <eacevedof@yahoo.es>
2018-10-20 13:55:16 +0200

    configurando el evento. implementando para esto la interface: import android.text.TextWatcher

--- app/src/main/java/es/theframework/adrkotlin/MainActivity.kt
+++ app/src/main/java/es/theframework/adrkotlin/MainActivity.kt
@@ -2,11 +2,14 @@ package es.theframework.adrkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.text.TextWatcher
 
-class MainActivity : AppCompatActivity(){
+class MainActivity() : AppCompatActivity(),TextWatcher {
 
     private var edtName: EditText? = null
     private var edtAge: EditText? = null
@@ -17,12 +20,19 @@ class MainActivity : AppCompatActivity(){
     private var sName: String? = null
     private var iAge = 0
 
+    constructor(parcel: Parcel) : this() {
+        sName = parcel.readString()
+        iAge = parcel.readInt()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //R.layout.activity_main: app/res/layout/activity_main.xml
         setContentView(R.layout.activity_main)
         this.load_inputs()
         //edtName?.addTextChangedListener(this)
+        edtName!!.addTextChangedListener(this)
+
     }//onCreate
 
     private fun load_inputs()
@@ -36,5 +46,17 @@ class MainActivity : AppCompatActivity(){
         //buttons
         btnGo = findViewById(R.id.btnGo) as Button
     }//load_inputs
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }//MainActivity
